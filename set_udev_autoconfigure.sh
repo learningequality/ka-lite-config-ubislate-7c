eae3ce41980882f8a0f98a76ba5c0105028351ad
Jamie Alexandre <jamalex@gmail.com>
2013-05-02 21:22:11 -0700

    Put alias in /usr/local/bin/ for autoconfig script.

--- set_udev_autoconfigure.sh
+++ set_udev_autoconfigure.sh
@@ -1,1 +1,2 @@
-echo 'ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTRS{idProduct}=="0003", RUN+="sh '`pwd`'/configure.sh"' > /etc/udev/rules.d/90-tablet-config.rules
+ln -s `pwd`/configure.sh /usr/local/bin/autoconfigure_ubislate.sh
+echo 'ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTRS{idProduct}=="0003", RUN+="sh /usr/local/bin/autoconfigure_ubislate.sh' > /etc/udev/rules.d/90-tablet-config.rules

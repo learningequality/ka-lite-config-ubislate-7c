075f79332dc22c031129783c3368f3363a3a1eb5
Jamie Alexandre <jamalex@gmail.com>
2013-05-02 22:14:58 -0700

    Change directories and sleep for 10 seconds before autoconfig.

--- configure.sh
+++ configure.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cd `dirname $0`
+sleep 10
 ./adb push ./com.android.browser_preferences.xml /data/data/com.android.browser/shared_prefs/com.android.browser_preferences.xml
 ./adb push ./wpa_supplicant.conf /data/misc/wifi/wpa_supplicant.conf
 ./adb push ./browser2.db /data/data/com.android.browser/databases/browser2.db
